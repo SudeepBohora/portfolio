e221a495d3f3831fdbac7ad96877cb3974708b15
SudeepBohora <34941540+SudeepBohora@users.noreply.github.com>
2018-09-24 20:19:34 +0545

    front page for the portfolio

---
+++ front.php
@@ -0,0 +1,25 @@
+<html>
+<header>
+<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Lobster" />
+<link rel="stylesheet" type="text/css" href="hello.css">
+
+</header>
+
+    <ul class="nav nav-pills">
+        <li>
+<a href="#">Sudeep Bohora</a>
+        </li>
+        <li class="pull-right">
+<a href="#">About</a>
+        </li>
+        <li class="pull-right">
+<a href="#">contact</a>
+        </li>
+</ul>
+<body>
+<div class ="pageOne">
+    <h1>Sudeep Bohora</h1>
+    
+</div>
+</body>
+</html>
